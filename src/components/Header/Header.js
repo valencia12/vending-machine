81f21a48a94e97e81211836f0a1e1f8848e8e629
valencia12 <00003417@uca.edu.sv>
2021-11-15 17:32:57 -0600

    Initial commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'; 
+import classes from './Header.module.css';
+
+
+
+export default function Header() {
+
+    return (
+        <div className = {` ${classes.Header} `}>
+            <h2>Hola</h2>
+            
+        </div>
+    )
+}
